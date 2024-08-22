217d1d096dd826cc0e12f91a49f47319ae351549
Yohance-Marcus <yohancemarcus47@gmail.com>
2024-08-21 20:03:05 -0400

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,18 @@
+age = 15
+we = 50 
+we = float (we)
+name = input("name:")
+name2 = input( "name2:")
+
+
+print(name , name2)
+a = int(input("enter a:")) 
+b = int(input("enter b:"))
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a**b)
+print(a!=b)
+print(a==b)
+print(a%b)
